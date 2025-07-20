5a5173ff8d33dc7abc66447426422f7fcbd7f0e4
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-07-20 21:35:52 +0530

    Create PROGRAM 5C.cpp

---
+++ PROGRAM 5C.cpp
@@ -0,0 +1,49 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: FIND LARGEST NUMBER AMONG THREE NUMBERS USING NESTED IF-ELSE (EXPERIMENT 5C)
+
+// CODE:
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "ENTER THE THREE NUMBERS: ";
+    cin >> a >> b >> c;
+
+    if (a > b) {
+        if (a > c) {
+            cout << a << " IS THE LARAGEST NUMBER." << endl;
+        } else {
+            cout << c << " IS THE LARAGEST NUMBER." << endl;
+        }
+    } else {
+        if (b > c) {
+            cout << b << " IS THE LARAGEST NUMBER." << endl;
+        } else {
+            cout << c << " IS THE LARAGEST NUMBER." << endl;
+        }
+    }
+
+    return 0;
+}
+
+// OUTPUT:
+
+// ENTER THE THREE NUMBERS: 1
+// 2
+// 3
+// 3 IS THE LARAGEST NUMBER.
+
+// ENTER THE THREE NUMBERS: -4
+// 0
+// 1
+// 1 IS THE LARAGEST NUMBER.
+
+
+
+
+
