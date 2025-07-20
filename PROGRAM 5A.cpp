ba7f4e8012aa5b466e3baf62a560799eb31e2a8b
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-07-20 21:31:08 +0530

    Update PROGRAM 5A.cpp

--- PROGRAM 5A.cpp
+++ PROGRAM 5A.cpp
@@ -1,8 +1,32 @@
 // NAME: YASH SHRIVASTAVA
 // PRN: 24070123137
 // BATCH: E&TC-B2
-// TITLE:  (EXPERIMENT: 5A)
+// TITLE: CHECK WHETHER NUMBER IS EVEN OR ODD (EXPERIMENT: 5A)
 
 
-// CODE:
+// CODE: 
+
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    
+    cout<<"ENTER THE NUMBER:";
+    cin>>number;
+    
+    if(number%2 == 0) {
+        cout<<number<<"IS EVEN."<<endl;
+    }
+    else {
+        cout<<number<<"IS ODD."<<endl;
+    }
+    return 0;
+}
+
+// OUTPUT:
+
+// ENTER THE NUMBER:4
+// 4IS EVEN.
+// ENTER THE NUMBER:7
+// 7IS ODD.
 
